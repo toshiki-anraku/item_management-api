06dd647f780b614525fe9c41a08b8193e27d3640
yutasasao <ss.yuta@icloud.com>
2022-10-24 21:07:51 +0900

    プリズマでのDB構築

---
+++ prisma/migrations/20221023132615_init/migration.sql
@@ -0,0 +1,53 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `User`;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `password` VARCHAR(32) NOT NULL,
+    `mail` VARCHAR(255) NOT NULL,
+    `reset_url` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `items` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `genre_id` INTEGER NOT NULL,
+    `name` VARCHAR(64) NOT NULL,
+    `status` INTEGER NOT NULL,
+    `img_path` VARCHAR(255) NOT NULL,
+    `priority` INTEGER NOT NULL,
+    `memo` TEXT NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `genres` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `items` ADD CONSTRAINT `items_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `items` ADD CONSTRAINT `items_genre_id_fkey` FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
